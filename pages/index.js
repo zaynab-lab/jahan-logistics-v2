a6922e98c8a95b5d3d7ce99efcd6ab281f906cbd
Abdallah Mobarak <75333513+zaynab-lab@users.noreply.github.com>
2022-09-05 17:24:17 +0300

    hero section

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,44 @@
 import PageLayout from "@/components/PageLayout";
+import IranMap from "@/public/img/svg/IranMap";
+import { styles } from "@/public/js/styles";
 
 export default function IndexPage() {
   return (
     <>
       <PageLayout pageName="">
-        <div>
-          <div className="pageContent">Home</div>
-        </div>
+        <section className="heroSection">
+          <h1 className="title">
+            From <span className="emph">IRAN</span> to All Over the{" "}
+            <span className="emph">World</span>
+          </h1>
+          <div className="map">
+            <IranMap />
+          </div>
+        </section>
       </PageLayout>
+      <style jsx>{`
+        .heroSection {
+          padding: 2rem;
+          ${styles.flex}
+          ${styles.flexAligncenter}
+          flex-wrap:wrap;
+          gap: 2rem;
+        }
+        .map {
+          width: 80%;
+          margin: auto;
+          flex: 1 1 30rem;
+          max-width: 36rem;
+        }
+        .title {
+          flex: 1 1 20rem;
+          text-align: center;
+          font-size: 2.2rem;
+        }
+        .emph {
+          color: ${styles.primaryColor};
+        }
+      `}</style>
     </>
   );
 }
