028f8377a2e06a1801f898c8ad918ba5c5bf1397
Abdallah Mobarak <75333513+zaynab-lab@users.noreply.github.com>
2022-09-09 10:26:00 +0300

    wtsIcon

--- components/atom/WtsFloat.js
+++ components/atom/WtsFloat.js
@@ -7,15 +7,21 @@ export default function () {
     <>
       <Link href="https://wa.me/+9613751568">
         <div className="floatwts">
-          <FaWhatsapp />
+          <img
+            width="100%"
+            src="./img/png/WhatsApp_icon.png"
+            alt={<FaWhatsapp />}
+          />
         </div>
       </Link>
       <style jsx>{`
         .floatwts {
-          background: green;
+          color: green;
           position: fixed;
           bottom: 2rem;
           right: 2rem;
+          width: 5rem;
+          height: 5rem;
           font-size: 2.6rem;
           color: white;
           ${styles.flex};
@@ -23,6 +29,7 @@ export default function () {
           border-radius: 50%;
           padding: 0.5rem;
           cursor: pointer;
+          z-index: 20;
         }
       `}</style>
     </>
